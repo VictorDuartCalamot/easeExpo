0986fd238e9abf10ed78b24ec87568b0b629e4c2
jGM2002 <josue.matta@ieselcalamot.com>
2024-03-12 15:39:19 +0100

    main update

--- ease/App.js
+++ ease/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { LoginScreen } from './src/screens/login_screen';
+import LoginScreen  from './src/screens/login_screen';
 
 export default function App() {
   return (
