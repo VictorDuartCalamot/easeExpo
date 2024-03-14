9a7bf7c48d1a14490236a6086198d21cad8562b4
jGM2002 <josue.matta@ieselcalamot.com>
2024-03-14 16:37:22 +0100

    try

--- ease/src/screens/login_screen.js
+++ ease/src/screens/login_screen.js
@@ -1,70 +1,27 @@
-import {Text, View, StyleSheet,ImageBackground,TextInput,Image,Button} from 'react-native'
-import React from 'react'
-import Register from './register_screen';
-import Home from './home_screen';
+import React from "react";
+import { Text, View, ImageBackground, TextInput, Image, TouchableOpacity } from "react-native";
 
-
-
-
-const LoginScreen = ({navigation}) =>{
-  return(
-    <View style={styles.container}>
-      <ImageBackground source={require("../pictures/fondo.png")}
-            style={styles.ImageBackground}>
-      <View style={styles.inputContainer}>
-        <Image source={require("../pictures/logo.png")}
-          style={styles.ImageLogo}>
-        </Image>
-          <TextInput placeholder="Enter Email or username"/>
-          <View style={styles.border}/>
-          <TextInput placeholder ="Password" secureTextEntry/>
-          <View style={styles.border}/>
-          
-          <Text style={{marginTop:15,color:"blue" }} onPress={() =>navigation.navigate(Register)}
-          >Don't have an account?</Text>
-
-          <Text style={{marginTop:15,color:"blue" }}>Don't remember your password?</Text>
-          <Text style={{marginTop:15,color:"blue" }} onPress={() =>navigation.navigate(Home)}
-          >Salto de pagina</Text>
-
-          <Text>iniciar sesion</Text>
-          {/**<Button>iniciar sesion</Button> */}
-      </View>
-      </ImageBackground>
-    </View>
-  );
+const LoginScreen = ({ navigation }) => {
+    return (
+        <View className="flex-1">
+            <ImageBackground source={require('../pictures/verde1.jpg')} className="h-full px-20 items-center">
+                <View className="h-450 w-full bg-white rounded-20 justify-center mt-170 px-25">
+                    <Image source={require('../pictures/logo.png')} className="w-25 h-25 self-center rounded-30 mb-20" />
+                    <TextInput placeholder="Enter Email or username" className="border-b-1 border-gray self-center w-full mb-4" />
+                    <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+                        <Text className="mt-4 text-blue">Don't have an account?</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Text className="mt-4 text-blue">Don't remember your password?</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+                        <Text className="mt-4 text-blue">prueba home</Text>
+                    </TouchableOpacity>
+                    <Text className="mt-4 text-blue">Iniciar sesion</Text>
+                </View>
+            </ImageBackground>
+        </View>
+    );
 };
 
 export default LoginScreen;
-const styles = StyleSheet.create({
-    container:{
-      flex:1
-    },
-    ImageBackground:{
-      height:"100%",
-      paddingHorizontal: 20,
-      alignItems:'center'
-    },
-    inputContainer:{
-      height: 450,
-      width:"100%",
-      backgroundColor:"white",
-      borderRadius:20,
-      justifyContent:"center",
-      marginTop: 170,
-      paddingHorizontal:25,
-    },
-    border:{
-      width:"100%",
-      backgroundColor:"gray",
-      height:1,
-      alignSelf:"center",
-    },
-    ImageLogo:{
-      width:100,
-      height:100,
-      alignSelf:"center",
-      borderRadius:30,
-      marginBottom:20,
-    }
-  })
