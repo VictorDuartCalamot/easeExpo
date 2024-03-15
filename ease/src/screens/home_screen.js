856d26e06b22cc576c3749e3253af5827422524c
Josue <estebanjosuematta@gmail.com>
2024-03-15 20:19:25 +0100

    drawMenu

--- ease/src/screens/home_screen.js
+++ ease/src/screens/home_screen.js
@@ -1,37 +1,14 @@
 import * as React from 'react';
-import { Text, View, TouchableOpacity, DrawerLayoutAndroid, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import DrawerMenu from '../components/DrawerMenu';
 
 function HomeScreen({ navigation }) {
-  const drawerRef = React.useRef(null);
-
-  const openDrawer = () => {
-    drawerRef.current.openDrawer();
-  }
-
-  const navigateToLogin = () => {
-    navigation.navigate('Login');
-  };
-
   return (
-    <DrawerLayoutAndroid
-      ref={drawerRef}
-      drawerWidth={250}
-      drawerPosition='left'
-      renderNavigationView={() => (
-        <View style={styles.drawer}>
-          <Text style={styles.menuTitle}>Menú</Text>
-        </View>
-      )}
-    >
+    <DrawerMenu navigation={navigation}>
       <View style={styles.container}>
-        <TouchableOpacity onPress={openDrawer} style={styles.menuButton}>
-          <Text style={styles.menuIcon}>☰</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={navigateToLogin} style={styles.loginButton}>
-          <Text style={styles.loginText}>Login</Text>
-        </TouchableOpacity>
+        <Text style={styles.title}>HomeScreen</Text>
       </View>
-    </DrawerLayoutAndroid>
+    </DrawerMenu>
   );
 }
 
@@ -40,35 +17,7 @@ const styles = StyleSheet.create ({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  drawer: {
-    flex: 1,
-    backgroundColor: '#fff',
-    padding: 20,
-  },
-  menuTitle: {
-    fontSize: 20,
-    marginBottom: 10,
-  },
-  menuButton: {
-    position: 'absolute',
-    left: 10,
-    top: 10,
-  },
-  menuIcon: {
-    fontSize: 20,
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 20,
-  },
-  loginButton: {
-    position: 'absolute',
-    bottom: 20,
-  },
-  loginText: {
-    fontSize: 18,
-    color: 'blue',
+    backgroundColor: '#7FFF00',
   },
 });
 
