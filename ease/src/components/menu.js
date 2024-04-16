acbbef476c19e7736e6a1ad99f82c170a7c7c638
ivan <ivan.villacencio@ieselcalamot.com>
2024-04-16 17:07:38 +0200

    config menu

--- ease/src/components/menu.js
+++ ease/src/components/menu.js
@@ -1,74 +1,61 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Platform, View, StyleSheet } from "react-native";
 import HomeScreen from "../screens/home_screen";
 import SettingsScreen from "../screens/settings_screen";
 import SummaryScreen from "../screens/summary_screen";
 
-const Tab = createBottomTabNavigator();
-const Drawer = createDrawerNavigator();
+const Tab = Platform.OS === 'web' ? createMaterialTopTabNavigator() : createBottomTabNavigator();
 
 const Menu = () => {
-  if (Platform.OS === "web") {
-    return (
-      <View style={styles.container}>
-        <Drawer.Navigator>
-          <Drawer.Screen name="Category" component={HomeScreen} />
-          <Drawer.Screen name="Setting" component={SettingsScreen} />
-          <Drawer.Screen name="Summary" component={SummaryScreen} />
-        </Drawer.Navigator>
-      </View>
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        <Tab.Navigator>
-          <Tab.Screen
-            name="Category"
-            component={HomeScreen}
-            options={{
-              tabBarLabel: "Category",
-              tabBarIcon: ({ color, size }) => (
-                <MaterialIcons name="data-usage" size={size} color={color} />
-              ),
-              headerShown: false,
-            }}
-          />
-          <Tab.Screen
-            name="Setting"
-            component={SettingsScreen}
-            options={{
-              tabBarLabel: "Setting",
-              tabBarIcon: ({ color, size }) => (
-                <MaterialIcons name="settings" size={size} color={color} />
-              ),
-              headerShown: false,
-            }}
-          />
-          <Tab.Screen
-            name="Summary"
-            component={SummaryScreen}
-            options={{
-              tabBarLabel: "Summary",
-              tabBarIcon: ({ color, size }) => (
-                <MaterialIcons name="description" size={size} color={color} />
-              ),
-              headerShown: false,
-            }}
-          />
-        </Tab.Navigator>
-      </View>
-    );
-  }
-};
+    return(
+        <View style={styles.container}>
+            <Tab.Navigator>
+                <Tab.Screen
+                    name="Category"
+                    component={HomeScreen}
+                    options={{
+                        tabBarLabel: 'Category',
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialIcons name="data-usage" size={size} color={color}/>
+                        ),
+                        headerShown: false
+                    }}
+                />
+                <Tab.Screen
+                    name="Setting"
+                    component={SettingsScreen}
+                    options={{
+                        tabBarLabel: 'Setting',
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialIcons name="settings" size={size} color={color}/>
+                        ),
+                        headerShown: false
+                    }}
+                />
+                <Tab.Screen
+                    name="Summary"
+                    component={SummaryScreen}
+                    options={{
+                        tabBarLabel: 'Summary',
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialIcons name="description" size={size} color={color}/>
+                        ),
+                        headerShown: false
+                    }}
+                />
+            </Tab.Navigator>
+        </View>
+    )
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "flex-start",
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+    },
 });
 
 export default Menu;
